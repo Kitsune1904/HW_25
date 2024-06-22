4e273a4bf37ed7b54374b4f59f210078ccc1d022
Kitsune1904 <annagumiho190423@gmail.com>
2024-06-22 22:08:02 +0300

    add full proj

---
+++ main.js
@@ -0,0 +1,20 @@
+let ladder = {
+    step: 0,
+    up: function () {
+        this.step++;
+        return this;
+    },
+    down: function () {
+        this.step--;
+        return this;
+    },
+    showStep: function () {
+        alert(this.step);
+        return this;
+    }
+};
+
+
+result = ladder.up().up().up().up().up().down().showStep();
+
+document.write(result.step)
