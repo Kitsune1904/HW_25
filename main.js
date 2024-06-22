0fa9338980a3c3019905736bc2c0da09dae1ce69
Kitsune1904 <annagumiho190423@gmail.com>
2024-06-22 22:13:42 +0300

    formating

--- main.js
+++ main.js
@@ -1,20 +1,19 @@
 let ladder = {
-    step: 0,
-    up: function () {
-        this.step++;
-        return this;
-    },
-    down: function () {
-        this.step--;
-        return this;
-    },
-    showStep: function () {
-        alert(this.step);
-        return this;
-    }
+  step: 0,
+  up: function () {
+    this.step++;
+    return this;
+  },
+  down: function () {
+    this.step--;
+    return this;
+  },
+  showStep: function () {
+    alert(this.step);
+    return this;
+  },
 };
 
+ladder.up().up().up().up().up().down().showStep();
 
-result = ladder.up().up().up().up().up().down().showStep();
-
-document.write(result.step)
+document.write(ladder.step);
